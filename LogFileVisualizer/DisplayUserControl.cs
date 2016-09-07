7817f684e0b9210abf26ce3e129f693ca2b914fe
Brian Hansen <brian@tf3604.com>
2016-09-07 07:54:21 -0500

    Add mapping objects.

--- LogFileVisualizer/DisplayUserControl.cs
+++ LogFileVisualizer/DisplayUserControl.cs
@@ -12,9 +12,26 @@ namespace LogFileVisualizer
 {
     public partial class DisplayUserControl : UserControl
     {
+        private Dictionary<Button, ColorComboBox> _buttonComboBoxMapping;
+        private Dictionary<Button, string> _buttonSettingsPropertyMapping;
+
         public DisplayUserControl()
         {
             InitializeComponent();
+
+            _buttonComboBoxMapping = new Dictionary<Button, ColorComboBox>()
+            {
+                { activeVlfCustomButton, activeVlfColorComboBox },
+                { currentVlfCustomButton, currentVlfColorComboBox },
+                { inactiveVlfCustomButton, inactiveVlfColorComboBox }
+            };
+
+            _buttonSettingsPropertyMapping = new Dictionary<Button, string>()
+            {
+                { activeVlfCustomButton, "ActiveVlfColor" },
+                { currentVlfCustomButton, "CurrentVlfColor" },
+                { inactiveVlfCustomButton, "InactiveVlfColor" }
+            };
         }
     }
 }
